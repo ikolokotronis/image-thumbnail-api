5a17842d93e87ec52df053fa1a5767e97518f517
ikolokotronis <ioanniskolokotronis1@gmail.com>
2022-06-17 11:14:51 +0200

    user & tier models

--- image_rest_api/users/models.py
+++ image_rest_api/users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    tier = models.ForeignKey('Tier', on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Tier(models.Model):
+    name = models.CharField(max_length=255)
+    thumbnail_height = models.IntegerField()
+    presence_of_original_file_link = models.BooleanField()
+    ability_to_fetch_expiring_link = models.BooleanField()
+
+    def __str__(self):
+        return self.name
