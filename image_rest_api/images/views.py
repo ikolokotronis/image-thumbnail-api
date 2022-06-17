ab801e2fb9e4d3a97d72ed56fb9602cfbd8ea12c
ikolokotronis <ioanniskolokotronis1@gmail.com>
2022-06-17 15:30:39 +0200

    bugfix

--- image_rest_api/images/views.py
+++ image_rest_api/images/views.py
@@ -30,7 +30,7 @@ class ImageView(APIView):
         if serializer.is_valid():
             serializer.save()
             if user.tier.name == 'Basic':
-                original_image_relative_path = 'media/thumbnails/' + os.path.basename(image_instance.original_image.path)
+                original_image_relative_path = 'media/images/' + os.path.basename(image_instance.original_image.path)
                 image = PILImage.open(image_instance.original_image.path)
                 image.thumbnail((200, 200))
                 image.save(original_image_relative_path + '_thumbnail200.png')
