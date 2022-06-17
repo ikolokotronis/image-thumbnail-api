2693b57a7d256187a3bfba6aeaf824277d32bc4f
ikolokotronis <ioanniskolokotronis1@gmail.com>
2022-06-17 13:12:33 +0200

    image model

--- image_rest_api/images/models.py
+++ image_rest_api/images/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/')
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.image.url
