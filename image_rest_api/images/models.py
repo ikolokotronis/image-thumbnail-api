c75d6258be966c6d575582504a473c3cbc0bbc60
ikolokotronis <ioanniskolokotronis1@gmail.com>
2022-06-20 13:45:21 +0200

    update img file save location

--- image_rest_api/images/models.py
+++ image_rest_api/images/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
 
+def upload_location(instance, filename, **kwargs):
+    """
+    Location for the image file
+    """
+    file_path = f'{instance.user.id}/images/{filename}'
+    return file_path
+
+
 class Image(models.Model):
     user = models.ForeignKey('users.User', on_delete=models.CASCADE)
-    original_image = models.ImageField(upload_to='images/')
+    original_image = models.ImageField(upload_to=upload_location)
     date_added = models.DateTimeField(auto_now_add=True)
+    expiration_time = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.original_image.url
